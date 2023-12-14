440b67edafe422ea149c009a356656d032c92cee
mimimya <sj.ella1030@gmail.com>
2023-12-14 10:47:34 +0900

    하얀 박스 레이아웃 추가

---
+++ src/layouts/WhiteBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WhiteBox() {
+	return (
+		<div 
+			className='fixed z-10 w-1/2 p-8 m-0 transform -translate-x-1/2 -translate-y-1/2 border-none rounded shadow-xl top-1/2 left-1/2 bg-white'>
+		</div>
+	);
+}
+
+export default WhiteBox;
