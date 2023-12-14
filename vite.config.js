0e8be2bd109d5666affe7f5eeb8a365de25a2e6d
mimimya <sj.ella1030@gmail.com>
2023-12-14 10:09:07 +0900

    vite 절대 경로 설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	alias: [
+		// @로 작성할 경우 /src로 대체되도록 
+		{ find: '@', replacement: '/src' }
+	]
+});
