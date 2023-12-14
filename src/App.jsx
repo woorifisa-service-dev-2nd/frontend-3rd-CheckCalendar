983d90236fefb78a19e5833ad062173507e629d8
rlaxoqkf <rlaxoqkf96@gmail.com>
2023-12-14 10:36:48 +0900

    App.jsx 기본화면 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import { useState } from 'react';
+import DefaultLayout from './layouts/DefaultLayout';
 
 function App() {
 
 	return (
-		<>
-			
-		</>
+		<DefaultLayout>
+			<div id='App' className='flex justify-between' >
+				<div id='side' className='border-[1px]'>
+					<div id='status' className='border-[1px]'>
+						work
+					</div>
+					<div id='cal' className='border-[1px]'>
+						달력
+					</div>
+				</div>
+				<div  id='body' className='border-[1px]'>
+					<div id='header' className='border-[1px]'>
+						제목, 날짜
+					</div>
+					<div id='list con' className='border-[1px]'>
+						<div id='list item'> 리스트들 </div>
+					</div>
+				</div>
+
+			</div>
+		</DefaultLayout>	
 	);
 }
 
