a0d1079377412309dd62b0b83636566ab1a7f3c5
HyeJin <hjhong9801@naver.com>
2023-12-14 18:00:59 +0900

    폰트 수정

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -34,7 +34,7 @@ const Title = ({ onAdd, date }) => {
 			<button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-600 group-hover:from-cyan-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
 				onClick={onAddTodo}
 				disabled={!checkAdd}>
-				<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 ">
+				<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-xl">
 					추가
 				</span>
 			</button>
